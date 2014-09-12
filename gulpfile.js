412402bed2bd67c86f12d40c634e7efcbe06a061
Federico Giovagnoli <giovagnolif@ryanair.com>
2014-09-12 08:52:41 +0100

    Added version bumping task. Upgraded dependencies versions. Added dependencies badges.

--- gulpfile.js
+++ gulpfile.js
@@ -17,10 +17,14 @@ var
   defineModule = require('gulp-define-module'),
   declare = require('gulp-declare'),
   concat = require('gulp-concat'),
+  git = require('gulp-git'),
+  bump = require('gulp-bump'),
+  tag_version = require('gulp-tag-version'),
   server = require('./app'),
   serverport = 5000,
   lrport = null,
-  cleanTmp;
+  cleanTmp,
+  inc;
 
 
 /* Helpers ----------------------------------------------------------------- */
@@ -224,6 +228,19 @@ gulp.task('watch', ['serve'], function () {
   gulp.watch('public/**/*', ['reload']);
 });
 
+
+/* Version bumping --------------------------------------------------------- */
+inc = function(importance) {
+  return gulp.src(['./package.json'])
+  .pipe(bump({type: importance}))
+  .pipe(gulp.dest('./'))
+  .pipe(git.commit('bumps package version'))
+  .pipe(tag_version()) ;
+};
+
+gulp.task('patch', function() { return inc('patch'); });
+gulp.task('feature', function() { return inc('minor'); });
+gulp.task('release', function() { return inc('major'); });
 
 
 // do not change these tasks unless you really know what you are doing
